2d69e34a7f7d1d6f32a4db6c15974bf7be9b83d7
ziv cohen <zivcohen1999@gmail.com>
2020-03-21 19:31:11 +0200

    fix test for floor

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,13 @@ int main(){
     s1.printList();
     std::cout << "FloorCheck: " << std::endl;
     Floor f1(0,2,2);
-    for(int i=0; i<2; i++){
-        for(int j=0; j<2; j++){
-            Container c1(10*i + j, "ISRAE", "CSQU3054383");
-            f1.set(i,j,&c1);
-        }
-    }
+    Container c2(1, "ISRAE", "CSQU3054383");
+    Container c3(2, "ISRAE", "CSQU3054383");
+    Container c4(3, "ISRAE", "CSQU3054383");
+    f1.set(0,0,&c1);
+    f1.set(0,1,&c2);
+    f1.set(1,0,&c3);
+    f1.set(1,1,&c4);
     for(int i=0; i<2; i++){
         for(int j=0; j<2; j++){
             std::cout << f1.get(i,j)->getWeight() << std::endl;
