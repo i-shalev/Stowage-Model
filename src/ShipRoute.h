d535f5146047353d3370acf4f03a9b826f115d0c
itay <itayshalev2@gmail.com>
2020-03-21 19:02:27 +0200

    add ShipRoute

---
+++ src/ShipRoute.h
@@ -0,0 +1,28 @@
+//
+// Created by itay on 21/03/2020.
+//
+#include <iostream>
+#include <vector>
+
+#ifndef STOWAGE_MODEL_SHIPROUTE_H
+#define STOWAGE_MODEL_SHIPROUTE_H
+
+using namespace std;
+class ShipRoute {
+    private:
+        vector<string> dstList;
+
+    public:
+        ShipRoute(vector<string> _dstList);
+        ShipRoute();
+
+        void addDstAtEnd(string dst);
+        void deleteFirst();
+        string getHead();
+        void printList();
+
+};
+bool checkDestination(string dest);
+
+
+#endif //STOWAGE_MODEL_SHIPROUTE_H
