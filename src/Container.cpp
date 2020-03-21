e22103542b7e1b8fcf327effe561dc60a4f3735d
itay <itayshalev2@gmail.com>
2020-03-21 15:21:57 +0200

    add Container class

---
+++ src/Container.cpp
@@ -0,0 +1,12 @@
+//
+// Created by itay on 21/03/2020.
+//
+
+#include "Container.h"
+
+public class Container {
+    private:
+        int weight;
+        string dest;
+        string id;
+};
---
+++ src/Container.h
@@ -0,0 +1,14 @@
+//
+// Created by itay on 21/03/2020.
+//
+
+#ifndef STOWAGE_MODEL_CONTAINER_H
+#define STOWAGE_MODEL_CONTAINER_H
+
+
+class Container {
+
+};
+
+
+#endif //STOWAGE_MODEL_CONTAINER_H
