db00277c31809a8b2f2ae9daf7e9bcc47ad714e2
ziv cohen <zivcohen1999@gmail.com>
2020-04-03 16:06:20 +0300

    change floor to use vectors

--- src/Floor.cpp
+++ src/Floor.cpp
@@ -8,41 +8,35 @@ Floor::Floor(int index, int length, int width, vector<vector<int>> blocks) {
     this->index = index;
     this->length=length;
     this->width=width;
-    this->floorMap = new Container**[length];
-
+    this->floorMap = new vector<vector<Container*>>(length, vector<Container*>(width, nullptr));
     int wid, len;
-
     for(int i=0; i<length; i++){
-        floorMap[i] = new Container*[width];
         for(int j=0; j<width; j++){
-            floorMap[i][j] = nullptr;
+            floorMap->at(i).push_back(nullptr);
         }
     }
-
     for(auto & block : blocks) {
         wid = block.at(0);
         len = block.at(1);
-        floorMap[wid][len] = new Container(-1, "blocked", "blocked", true);
+        if(wid!=-1 & len!=-1)
+            floorMap->at(wid).at(len) = new Container(-1, "blocked", "blocked", true);
     }
 
 
 }
 Floor::~Floor() {
-    for(int i=0; i<length; i++){
-        delete[] floorMap[i];
-    }
-    delete[] floorMap;
+    delete floorMap;
 }
 
 Container* Floor::getContainerAtPosition(int i, int j){
     if(i<0 | j<0 | i>length | j>width)
         return nullptr;
-    return this->floorMap[i][j];
+    return (*this->floorMap)[i][j];
 }
 
 void Floor::setContainerAtPosition(int i, int j, Container* value) {
     if(i<0 | j<0 | i>length | j>width)
         return;
-    floorMap[i][j] = value;
+    (*floorMap)[i][j] = value;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ int main(){
 //    s1->addDstAtEnd("ISAAA");
 //    s1->addDstAtEnd("ZIVCO");
 //    s1->addDstAtEnd("ABCDE");
-    int numFloors=0 , length=0, width=0;
+   /* int numFloors=0 , length=0, width=0;
     string path = R"(C:\Users\itay\Desktop\exmaple1.csv)";
     vector<vector<vector<int>>> blocks = readShipPlan(path, numFloors, length, width);
     for(auto & block : blocks) {
@@ -25,7 +25,22 @@ int main(){
                 std::cout << k << std::endl;
             }
         }
-    }
+    }*/
+   vector<vector<int>> vec;
+   Floor* f1 = new Floor(0,2,2, vec);
+   Container* c2 = new Container(1005, "ISRAE", "CSQU3054383");
+   Container* c3 = new Container(2, "ISRAE", "CSQU3054383");
+   Container* c4 = new Container(3, "ISRAE", "CSQU3054383");
+   f1->setContainerAtPosition(0,0,c2);
+   f1->setContainerAtPosition(1,0,c3);
+   f1->setContainerAtPosition(0,1,c4);
+   std::cout << f1->getContainerAtPosition(0,0)->getWeight() << std::endl;
+   std::cout << f1->getContainerAtPosition(1,0)->getWeight() << std::endl;
+   std::cout << f1->getContainerAtPosition(0,1)->getWeight() << std::endl;
+    delete f1;
+    delete c2;
+    delete c3;
+    delete c4;
 
 //    Floor* f1 = new Floor(0,2,2);
 //    Container* c2 = new Container(1005, "ISRAE", "CSQU3054383");
