37cd73ada17e6b4b47a220490e600726ef63ec79
ziv cohen <zivcohen1999@gmail.com>
2020-05-10 09:40:30 +0300

    move registration to simulation

--- src/Simulation/AlgorithmRegistration.cpp
+++ src/Simulation/AlgorithmRegistration.cpp
@@ -2,8 +2,8 @@
 // Created by zivco on 10/05/2020.
 //
 
-#include "AlgorithmRegistration.h"
-#include "AlgorithmRegistrar.h"
+#include "../Common/AlgorithmRegistration.h"
+#include "../Common/AlgorithmRegistrar.h"
 
 AlgorithmRegistration::AlgorithmRegistration(std::function<std::unique_ptr<AbstractAlgorithm>()> algorithmFactory) {
     AlgorithmRegistrar::getInstance().registerAlgorithm(algorithmFactory);
