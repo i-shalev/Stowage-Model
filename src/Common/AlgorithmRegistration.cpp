c4e4117f65a99f0b29a91c5e8c19d8b9b39fcbe2
ziv cohen <zivcohen1999@gmail.com>
2020-05-10 09:33:18 +0300

    2nd half of the prev commit

--- src/Common/AlgorithmRegistration.cpp
+++ src/Common/AlgorithmRegistration.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "AlgorithmRegistration.h"
+#include "AlgorithmRegistrar.h"
 
 AlgorithmRegistration::AlgorithmRegistration(std::function<std::unique_ptr<AbstractAlgorithm>()> algorithmFactory) {
-    AlgorithmRegistrar::getInstance().registerAlgorithm(algorithmFactory, name);
+    AlgorithmRegistrar::getInstance().registerAlgorithm(algorithmFactory);
+}
