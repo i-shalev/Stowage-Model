1daf596bc30d34fc63acfcb03847c43dda897838
ziv cohen <zivcohen1999@gmail.com>
2020-05-18 17:13:03 +0300

    reduce functions length

--- src/Common/Crane.cpp
+++ src/Common/Crane.cpp
@@ -77,17 +77,10 @@ result Crane::Move(const std::string& contId, int level, int i, int j, int toLev
 }
 
 int Crane::executeOperationList(const std::string& path, std::vector<std::string>& errors) {
-    int price = 0;
-    std::ifstream fin;
-    try{
-        fin.open(path, std::ios::in);
-    } catch (const std::exception& e) {
-        errors.push_back("Failed to open file");
-        return -1;
-    }
-    std::string line, id;
-    size_t sz, last_index = -1;
-    int level, i, j;
+    int price = 0; std::ifstream fin;
+    try{ fin.open(path, std::ios::in);}
+    catch (const std::exception& e) {errors.push_back("Failed to open file"); return -1;}
+    std::string line, id; size_t sz, last_index = -1; int level, i, j;
     while(getline(fin, line)) {
         if( line.at(0) == 'U'){
             id =  line.substr(3,11) ;
@@ -101,17 +94,13 @@ int Crane::executeOperationList(const std::string& path, std::vector<std::string
                 errors.push_back("Unload " + id +" " + std::to_string(level) + " " + std::to_string(i) + " " + std::to_string(j) + " is invalid operation.");
                 switch(rc){
                     case 2:
-                        errors.emplace_back("Error: index out of range");
-                        break;
+                        errors.emplace_back("Error: index out of range"); break;
                     case 5:
-                        errors.emplace_back("Error: cant unload container. container above");
-                        break;
+                        errors.emplace_back("Error: cant unload container. container above"); break;
                     case 6:
-                        errors.emplace_back("Error: there is other container in this place");
-                        break;
+                        errors.emplace_back("Error: there is other container in this place"); break;
                     case 7:
-                        errors.emplace_back("Error: there is no container in this place");
-                        break;
+                        errors.emplace_back("Error: there is no container in this place"); break;
                 }
                 return -1;
             }
@@ -129,20 +118,15 @@ int Crane::executeOperationList(const std::string& path, std::vector<std::string
                 errors.push_back("Load " + id +" " + std::to_string(level) + " " + std::to_string(i) + " " + std::to_string(j) + " is invalid operation.");
                 switch(rc){
                     case 1:
-                        errors.emplace_back("Error: cant load the container because the ship will not visit its destination");
-                       break;
+                        errors.emplace_back("Error: cant load the container because the ship will not visit its destination"); break;
                     case 2:
-                        errors.emplace_back("Error: index out of range");
-                        break;
+                        errors.emplace_back("Error: index out of range"); break;
                     case 3:
-                        errors.emplace_back("Error: cant load container. the location is full");
-                        break;
+                        errors.emplace_back("Error: cant load container. the location is full"); break;
                     case 4:
-                        errors.emplace_back("Error: cant load container. there is nothing below" );
-                        break;
+                        errors.emplace_back("Error: cant load container. there is nothing below" ); break;
                     case 6:
-                        errors.emplace_back("Error: cant find the container, wrong ID");
-                        break;
+                        errors.emplace_back("Error: cant find the container, wrong ID"); break;
                 }
                 return -1;
             }
@@ -167,37 +151,26 @@ int Crane::executeOperationList(const std::string& path, std::vector<std::string
                 " " + std::to_string(toLevel) + " " + std::to_string(toI) + " " + std::to_string(toJ) + " is invalid operation.");
                 switch(rc){
                     case 1:
-                        errors.emplace_back("Error: cant load the container because the ship will not visit its destination");
-                        break;
+                        errors.emplace_back("Error: cant load the container because the ship will not visit its destination"); break;
                     case 2:
-                        errors.emplace_back("Error: index out of range");
-                        break;
+                        errors.emplace_back("Error: index out of range"); break;
                     case 3:
-                        errors.emplace_back("Error: cant load container. the location is full");
-                        break;
+                        errors.emplace_back("Error: cant load container. the location is full"); break;
                     case 4:
-                        errors.emplace_back("Error: cant load container. there is nothing below" );
-                        break;
+                        errors.emplace_back("Error: cant load container. there is nothing below" ); break;
                     case 5:
-                        errors.emplace_back("Error: cant unload container. container above");
-                        break;
+                        errors.emplace_back("Error: cant unload container. container above"); break;
                     case 6:
-                        errors.emplace_back("Error: there is other container in this place");
-                        break;
+                        errors.emplace_back("Error: there is other container in this place"); break;
                     case 7:
-                        errors.emplace_back("Error: there is no container in this place");
-                        break;
+                        errors.emplace_back("Error: there is no container in this place"); break;
                 }
                 return -1;
             }
             price++;
         }
-        else if( line.at(0) == 'R'){
-            id =  line.substr(3,11);
-        }
-        else{
-            errors.emplace_back("wrong file format");
-        }
+        else if( line.at(0) == 'R'){ id =  line.substr(3,11);}
+        else{ errors.emplace_back("wrong file format");}
     }
     fin.close();
     return price;
