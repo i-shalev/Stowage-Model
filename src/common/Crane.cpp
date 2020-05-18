7ec47501b57293f9cb0629a1b7eea3df79ba2a75
ziv cohen <zivcohen1999@gmail.com>
2020-05-18 19:50:11 +0300

    print error if reject non exist id

--- src/common/Crane.cpp
+++ src/common/Crane.cpp
@@ -82,11 +82,9 @@ int Crane::executeOperationList(const std::string& path, std::vector<std::string
     catch (const std::exception& e) {errors.push_back("Failed to open file"); return -1;}
     std::string line, id; int level, i, j;
     while(getline(fin, line)) {
-        std::vector<std::string> vec;
-        getArgsFromInstruction(vec, line);
+        std::vector<std::string> vec; getArgsFromInstruction(vec, line);
         if(vec.size() != 2 and vec.size() != 5 and vec.size() != 8){
-            errors.emplace_back("Error: instruction wrong format (wrong number of parameters in line).");
-            return -1;
+            errors.emplace_back("Error: instruction wrong format (wrong number of parameters in line)."); return -1;
         }
         if(vec.size() == 2 && vec.at(0) == "R"){
             id =  vec.at(1);
@@ -94,12 +92,9 @@ int Crane::executeOperationList(const std::string& path, std::vector<std::string
         } else if (vec.size() == 5) {
             id =  vec.at(1);
             try{
-                level = std::stoi(vec.at(2));
-                i =  std::stoi(vec.at(3)) ;
-                j =  std::stoi(vec.at(4)) ;
+                level = std::stoi(vec.at(2)); i =  std::stoi(vec.at(3)) ; j =  std::stoi(vec.at(4)) ;
             } catch (const std::exception& e) {
-                errors.emplace_back("Error: instruction wrong format (not numbers).");
-                return -1;
+                errors.emplace_back("Error: instruction wrong format (not numbers)."); return -1;
             }
             if(vec.at(0) == "L"){
                 int rc = this->Load(id,level,i,j);
@@ -144,15 +139,10 @@ int Crane::executeOperationList(const std::string& path, std::vector<std::string
         } else if (vec.size() == 8 and vec.at(0) == "M") {
             int toLevel, toI, toJ;
             try{
-                level = std::stoi(vec.at(2));
-                i =  std::stoi(vec.at(3)) ;
-                j =  std::stoi(vec.at(4)) ;
-                toLevel = std::stoi(vec.at(5)) ;
-                toI = std::stoi(vec.at(6)) ;
-                toJ = std::stoi(vec.at(7)) ;
+                level = std::stoi(vec.at(2)); i =  std::stoi(vec.at(3)) ; j =  std::stoi(vec.at(4)) ;
+                toLevel = std::stoi(vec.at(5)) ; toI = std::stoi(vec.at(6)) ; toJ = std::stoi(vec.at(7)) ;
             } catch (const std::exception& e) {
-                errors.emplace_back("Error: instruction wrong format (not numbers).");
-                return -1;
+                errors.emplace_back("Error: instruction wrong format (not numbers)."); return -1;
             }
             int rc = this->Move(id,level,i,j, toLevel, toI, toJ);
             if(rc != SUCCESS){
