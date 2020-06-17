f8ebc5bd8e58f1054c244ff5f115bf9dcfb7f6b0
ziv cohen <zivcohen1999@gmail.com>
2020-06-17 09:35:26 +0300

    move Balancer.cpp to simulator and fix makefiles

--- src/simulator/WeightBalanceCalculator.cpp
+++ src/simulator/WeightBalanceCalculator.cpp
@@ -2,7 +2,7 @@
 // Created by itay on 05/04/2020.
 //
 
-#include "WeightBalanceCalculator.h"
+#include "../common/WeightBalanceCalculator.h"
 
 //the parameters are in comment in order to avoid unused variables warnings
 WeightBalanceCalculator::BalanceStatus WeightBalanceCalculator::tryOperation(char loadUnload, int kg, int x, int y) {
